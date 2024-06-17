14583619d0b1918379b30b356f173e7c230bd92b
javierfebres36 <164325528+javierfebres36@users.noreply.github.com>
2024-06-17 08:20:46 +0000

    First Commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name ? variables.name : "firsname"} ${
+    variables.lastName ? variables.lastName : "lasName"
+  }</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h3>${variables.role ? variables.role : "webdeveloper"}${
+    variables.country ? variables.country : ""
+  }${variables.city ? variables.city : ""}</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
